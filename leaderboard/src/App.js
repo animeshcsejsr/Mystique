639e72af53f90cac408b1d06ea636e7be41174b6
Animesh Mishra <cheersanimesh@gmail.com>
2021-10-05 21:04:23 +0530

    Added code for the leaderboard website

---
+++ leaderboard/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdminPanel from './pages/adminPanel'
+function App() {
+  return (
+      <AdminPanel />
+  );
+}
+
+export default App;
